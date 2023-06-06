d74c1de9ac0989dba872fe0c6dcceb1beaf25b23
Nazmulhasan032 <nh30898@gmail.com>
2023-06-06 22:41:11 +0600

    title and logo setup done

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -14,15 +14,8 @@ const Navbar = () => {
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
+                        <li><a>Item 2</a></li>
+                        
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
